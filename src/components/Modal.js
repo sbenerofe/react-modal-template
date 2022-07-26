bd9ac322dfffbfc16541e83a30f241993ff23f8c
Steven Benerofe <sbenerofe@gmail.com>
2022-07-26 16:51:29 -0700

    improve modal response

---
+++ src/components/Modal.js
@@ -0,0 +1,19 @@
+export default function Modal(props) {
+  function cancelHandler() {
+    props.onCancel();
+  }
+  function confirmHandler() {
+    props.onConfirm();
+  }
+  return (
+    <div className='modal'>
+      <p>Ae you sure</p>
+      <button className='btn btn--alt' onClick={cancelHandler}>
+        Cancel
+      </button>
+      <button className='btn' onClick={confirmHandler}>
+        Confirm
+      </button>
+    </div>
+  );
+}
